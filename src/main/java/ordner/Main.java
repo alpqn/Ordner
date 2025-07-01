431a498cd6040763b4ac2fe97b478a7cc6e59868
quonverbat <quonverbat@gmail.com>
2025-07-01 20:46:57 +0300

    Added logo

--- src/main/java/ordner/Main.java
+++ src/main/java/ordner/Main.java
@@ -3,6 +3,7 @@ package ordner;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class Main extends Application
 	{
 		var scene = new Scene(FXMLLoader.load(Main.class.getResource("fxml/App.fxml")));
 		stage.setTitle("Ordner");
+		stage.getIcons().add(new Image(getClass().getResource("icons/logo.png").toString()));
 		stage.setScene(scene);
 		stage.show();
 	}
